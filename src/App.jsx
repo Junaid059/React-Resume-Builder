77685a8f784c94618540ea16f43d27036bdfdc73
junaid khalid <junaidkhalid2001@gmail.com>
2024-10-09 20:33:57 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="my-4 px-3 bg-orange-600">
+    <Button>Hello Shadcn</Button>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
