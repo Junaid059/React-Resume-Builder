93772206e9ccd1e5c5e565e512cb96d5520125b2
junaid khalid <junaidkhalid2001@gmail.com>
2024-10-14 22:15:36 +0500

    completed add Resume component

--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { PlusSquare } from 'lucide-react';
+import { Loader2, PlusSquare } from 'lucide-react';
 import { Input } from '@/components/ui/input';
+import { v4 as uuidv4 } from 'uuid';
+import { useUser } from '@clerk/clerk-react';
+
 import {
   Dialog,
   DialogContent,
@@ -12,6 +15,20 @@ import {
 
 export default function AddResume() {
   const [openDialog, setOpenDialog] = useState(false);
+  const [resumeTitle, setresumeTitle] = useState('');
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+
+  const onCreate = () => {
+    setLoading(true);
+    const uuid = uuidv4();
+    const data = {
+      title: resumeTitle,
+      resumeId: uuid,
+      email: user?.primaryEmailAddress?.emailAddress,
+      userName: user?.fullName,
+    };
+  };
   return (
     <div>
       <div
@@ -26,12 +43,20 @@ export default function AddResume() {
             <DialogTitle>Create New Resume</DialogTitle>
             <DialogDescription>
               <p>Add a title for resume</p>
-              <Input className="mt-2" placeholder="Full stack Resume" />
+              <Input
+                className="mt-2"
+                placeholder="Full stack Resume"
+                onChange={(e) => setresumeTitle(e.target.value)}
+              />
             </DialogDescription>
             <div className="flex justify-end gap-5">
               <button onClick={() => setOpenDialog(false)}>Cancel</button>
-              <button className="bg-[#8B5CF6] text-white p-3 rounded-md hover:bg-[#6D3FDB] focus:ring-2 focus:ring-[#6D3FDB] transition-all duration-300 ease-in-out shadow-md hover:shadow-lg">
-                Create
+              <button
+                className="bg-[#8B5CF6] text-white p-3 rounded-md hover:bg-[#6D3FDB] focus:ring-2 focus:ring-[#6D3FDB] transition-all duration-300 ease-in-out shadow-md hover:shadow-lg"
+                disabled={!resumeTitle || loading}
+                onClick={() => onCreate()}
+              >
+                {loading ? <Loader2 className="animate-spin" /> : 'Create'}
               </button>
             </div>
           </DialogHeader>
